692a50d96de8745ddcf5e666ff5edb4d0ca48143
negiisha799-glitch <negiisha799@gmail.com>
2025-11-13 12:47:22 +0530

    Create percentage.c

---
+++ percentage.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  float m1 = 100; // math marks
+  float m2 = 77; // physics marks
+  float m3 = 92; // chem marks
+  float m4 = 90; // english marks
+  float m5 = 86; // economic marks
+  float p = (m1 + m2 + m3 + m4 + m5) / 5;
+  printf("percentage of 5 subjects is: %f", p);
+
+    return 0;
+}
